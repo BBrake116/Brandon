be2f2c3ab99d850912faad9f9b1abd0473cef00a
BBrake <Brandongb234@gmail.com>
2019-10-22 14:30:54 -0700

    ukj

--- variables/src/VariablesAndNames.java
+++ variables/src/VariablesAndNames.java
@@ -18,5 +18,30 @@ public class VariablesAndNames {
       System.out.println( "We can transport " + carpool_capacity + " people today." );
       System.out.println( "We have " + passengers + " to carpool today." );
       System.out.println( "We need to put about " + average_passengers_per_car + " in each car." );
+
+      int room;
+      double e_is_close_to;
+      String Computer_Science;
+
+      room = 113;
+      e_is_close_to = 2.71828;
+      Computer_Science = "Computer Science";
+
+       System.out.println("This is room #" + room);
+       System.out.println("e is close to" + " " + e_is_close_to);
+       System.out.println("I am learning a bit about" + " " + Computer_Science);
+
+      System.out.println("+----------------------------------------------------------------+");
+      System.out.println("| 1 |                             English III |        Ms. Lapan |");
+      System.out.println("| 2 |                             Precalculus |      Mrs. Gideon |");
+      System.out.println("| 3 |                            Music Theory |        Mr. Davis |");
+      System.out.println("| 4 |                           Biotechnology |       Ms. Palmer |");
+      System.out.println("| 5 |              Principles of Technology I |       Ms. Garcia |");
+      System.out.println("| 6 |                                Latin II |      Mrs. Barnett|");
+      System.out.println("| 7 |                          AP US  History |  Ms. Johannessen |");
+      System.out.println("| 8 |   Business Computer Information Systems |        Mr. James |");
+      System.out.println("+----------------------------------------------------------------+");
+
+
    }
 }
